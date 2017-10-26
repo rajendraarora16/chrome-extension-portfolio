f785881f9a81c0ac06cd5ce35d3cbbf04376d419
Rajendra arora <rajendraarora161992@gmail.com>
2017-10-26 08:45:09 +0530

    Chrome extension to show my portfolio

---
+++ popup.js
@@ -0,0 +1,7 @@
+document.addEventListener('DOMContentLoaded', function() {
+  var checkPageButton = document.getElementById('visitPage');
+  checkPageButton.onclick = function(){
+
+    console.log('Redirected to https://www.rajendraarora.com');
+  };
+}); 
